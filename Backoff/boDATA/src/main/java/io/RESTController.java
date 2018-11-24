f737629d134fba05cc6a15b222c079021b48afd8
brainswitch <brainswitch@gmail.com>
2018-11-24 10:47:50 +0100

    log trace

--- Backoff/boDATA/src/main/java/io/RESTController.java
+++ Backoff/boDATA/src/main/java/io/RESTController.java
@@ -227,6 +227,9 @@ public class RESTController
       tracelog.info("poicity:"+poicity);
       JSONObject j_poi = new JSONObject(poicity).getJSONObject("poi");
       String city = new JSONObject(poicity).getString("city");
+      POI poi = mapper.readValue(j_poi, POI.class);
+      List<POI> poi_list = new ArrayList<>();
+      poi_list.add(poi);
       tracelog.info("name:"+j_poi.get("display_name"));
       JSONArray array = new JSONArray();
       array.put(j_poi);
@@ -234,7 +237,7 @@ public class RESTController
       tracelog.info("Write POI(string):"+j_poi.toString());
       tracelog.info("Write POI(array-string):"+array.toString());
       tracelog.info("Write POI(array):"+array);
-      mapper.writeValue(new File(this.properties.getProperty("citiesPath") + city + "/pois/" + j_poi.getString("place_id") + ".json"), array);
+      mapper.writeValue(new File(this.properties.getProperty("citiesPath") + city + "/pois/" + j_poi.getString("place_id") + ".json"), poi_list);
       SavePOIs2DB.run(this.logger, city, this.dao, this.properties.getProperty("citiesPath") + city + "/pois/");
     }
     catch (Exception e)
